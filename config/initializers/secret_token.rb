813dd1babd2577c56feab958a05e8bf47339eabf
Bob Bhatti <bobbhatti@simransservices.co.uk>
2013-01-20 19:21:58 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Justonemorerep::Application.config.secret_token = '8c629b8cf09f38e438b8879e64a8e075fae954f9ee60646eb461a427a77180260ce6744f238e5838e502886d33448c6e1355931c9f6146c28ed8d3a4f4afc37f'
